8a22b8f2455daa353487d173c56c92aefb20cbaa
Victor Hugo Rosa Fonseca <victor.fonseca@tvmorena.com.br>
2025-07-10 10:27:16 -0400

    feat: adicionado contagem de perguntas em cada sala

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,14 +1,20 @@
-import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
-import { db } from "../../db/connection.ts";
-import { schema } from "../../db/schema/index.ts";
+import { count, eq } from "drizzle-orm"
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
+import { db } from "../../db/connection.ts"
+import { schema } from "../../db/schema/index.ts"
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
     app.get('/rooms', async () => {
         const results = await db.select({
             id: schema.rooms.id,
-            name: schema.rooms.name
-        }).from(schema.rooms).orderBy(schema.rooms.createdAt);
+            name: schema.rooms.name,
+            qiestionsCount: count(schema.questions.id)
+        })
+            .from(schema.rooms)
+            .leftJoin(schema.questions, eq(schema.questions.roomId, schema.questions.roomId))
+            .groupBy(schema.rooms.id, schema.rooms.name)
+            .orderBy(schema.rooms.createdAt)
 
-        return results;
-    });
-};
+        return results
+    })
+}
