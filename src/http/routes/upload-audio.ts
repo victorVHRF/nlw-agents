aea76e1d5dfb622c85cbdf40173bbe9b6cb105a0
Victor Hugo Rosa Fonseca <victor.fonseca@tvmorena.com.br>
2025-07-11 15:36:46 -0400

    feat: criado a rota de upload de audio

---
+++ src/http/routes/upload-audio.ts
@@ -0,0 +1,43 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
+import { z } from "zod/v4"
+import { db } from "../../db/connection.ts"
+import { schema } from "../../db/schema/index.ts"
+import { generateEmbeddings, transcribeAudio } from "../../services/gemini.ts"
+
+export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
+    app.post('/rooms/:roomId/audio', {
+        schema: {
+            params: z.object({
+                roomId: z.string()
+            }),
+
+        }
+    }, async (request, reply) => {
+        const { roomId } = request.params
+        const audio = await request.file()
+
+        if (!audio) {
+            throw new Error('Audio is required.')
+        }
+
+        const audioBuffer = await audio.toBuffer()
+        const audioToBase64 = audioBuffer.toString('base64')
+
+        const transcription = await transcribeAudio(audioToBase64, audio.mimetype)
+        const embeddings = await generateEmbeddings(transcription)
+
+        const result = await db.insert(schema.audioChunks).values({
+            roomId,
+            transcription,
+            embeddings
+        }).returning()
+
+        const chunck = result[0]
+
+        if (!chunck) {
+            throw new Error('Erro ao salvar chunck de audio.')
+        }
+
+        return reply.status(201).send({ chunckId: chunck.id })
+    })
+} 
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastifyCors from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
@@ -6,12 +7,15 @@ import { createQuestionRoute } from './http/routes/create-question.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomsQuestionsRoute } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
     origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -24,5 +28,6 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomsQuestionsRoute)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT }) 
